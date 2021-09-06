1d3242472c6b08fa851101df9a15bb7cfaba6900
useStateEthan <ethancampbellhodges@gmail.com>
2021-09-06 10:09:28 -0400

    completed task basic functions

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,48 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet, TextInput } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+export default function TabOneScreen({ navigation }: RootTabScreenProps<"TabOne">) {
+	const [username, setUsername] = React.useState("");
+	const [password, setPassword] = React.useState("");
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Log in / Sign up</Text>
+			<Text>Username</Text>
+			<TextInput
+				style={{ height: 40, width: 200, borderColor: "gray", borderWidth: 1, padding: 10, color: "white" }}
+				onChangeText={(text) => setUsername(text)}
+				value={username}
+			></TextInput>
+			<Text>Password</Text>
+			<TextInput
+				style={{ height: 40, width: 200, borderColor: "gray", borderWidth: 1, padding: 10, color: "white" }}
+				onChangeText={(text) => setPassword(text)}
+				value={password}
+			></TextInput>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		marginBottom: 40,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		marginBottom: 20,
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,32 +1,137 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet, TextInput, Button } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+
+const TESTING_TOKEN =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTM0ZjEyMjlhODM0ZTAwMTY1MjZiMWUiLCJpYXQiOjE2MzA4NTk1NTR9.TmCNPZcXbiHIji4e9SEAbolyI42LBLX38QzWz4KsH2U";
+
+interface oneTask {
+	__v: number;
+	_id: string;
+	completed: boolean;
+	description: string;
+	owner: string;
+}
 
 export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+	const [todos, setTodos] = React.useState<oneTask[]>([]);
+	const [input, setInput] = React.useState("");
+
+	React.useEffect(() => {
+		const getTasks = async () => {
+			const alltasks = await fetch("https://task-manager-api-v2.herokuapp.com/tasks", {
+				method: "GET",
+				cache: "default",
+				headers: {
+					Authorization: TESTING_TOKEN,
+					"Content-Type": "application/json",
+				},
+			});
+			const tasks: oneTask[] = await alltasks.json();
+			console.log(tasks);
+			setTodos(tasks);
+		};
+		getTasks();
+	}, []);
+
+	const handleItemAdd = async () => {
+		const taskToSave = { description: input, completed: false };
+		setInput("");
+		console.log(taskToSave);
+		try {
+			const savedTask = await fetch("https://task-manager-api-v2.herokuapp.com/tasks", {
+				method: "POST",
+				headers: {
+					Authorization: TESTING_TOKEN,
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(taskToSave),
+			});
+			const jsonTask = await savedTask.json();
+			setTodos([...todos, jsonTask]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleItemDelete = async (ele: oneTask) => {
+		setTodos((prev) => prev.filter((item) => item._id !== ele._id));
+		try {
+			await fetch("https://task-manager-api-v2.herokuapp.com/tasks/" + ele._id, {
+				method: "DELETE",
+				headers: {
+					Authorization: TESTING_TOKEN,
+					"Content-Type": "application/json",
+				},
+			});
+		} catch (e) {}
+	};
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>To Do</Text>
+			<View>
+				<TextInput style={styles.textInput} onChangeText={(text) => setInput(text)} value={input}></TextInput>
+				<Button title='add a to do' onPress={(press) => handleItemAdd()}>
+					Add
+				</Button>
+			</View>
+
+			<View>
+				{todos.map((ele, i) => {
+					return (
+						<View
+							key={i}
+							style={{
+								height: 40,
+								minWidth: 300,
+								borderColor: "gray",
+								borderWidth: 1,
+								display: "flex",
+								flexDirection: "row",
+								alignItems: "center",
+								padding: 1,
+								justifyContent: "space-evenly",
+							}}
+						>
+							<Text style={{ textAlign: "center" }}>{ele.description}</Text>
+							<Button title='Delete' onPress={() => handleItemDelete(ele)}></Button>
+						</View>
+					);
+				})}
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: "center",
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		marginTop: 20,
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: "80%",
+	},
+	textInput: {
+		height: 40,
+		width: 200,
+		borderColor: "gray",
+		borderWidth: 1,
+		padding: 10,
+		color: "white",
+		marginTop: 20,
+	},
+	toDoItem: {
+		height: 40,
+		width: 200,
+	},
 });
